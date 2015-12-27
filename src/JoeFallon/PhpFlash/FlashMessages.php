5221a9395ce2669a05db7ff0bfd2ecd8e91924b9
Joe Fallon <joseph.t.fallon@gmail.com>
2015-12-27 12:47:00 -0800

    The ability to store and retrieve by key was removed. A composer update was performed. Code clean-up was performed.

--- src/JoeFallon/PhpFlash/FlashMessages.php
+++ src/JoeFallon/PhpFlash/FlashMessages.php
@@ -3,13 +3,13 @@ namespace JoeFallon\PhpFlash;
 
 use JoeFallon\PhpSession\Session;
 
-/**
- * @author    Joseph Fallon <joseph.t.fallon@gmail.com>
- * @copyright Copyright 2014 Joseph Fallon (All rights reserved)
- * @license   MIT
- */
 class FlashMessages
 {
+    const INFO_SESSION_KEY    = 'flash_information_messages';
+    const SUCCESS_SESSION_KEY = 'flash_success_messages';
+    const WARNING_SESSION_KEY = 'flash_warning_messages';
+    const ERROR_SESSION_KEY   = 'flash_error_messages';
+
     /** @var  array */
     private $_infoMessages;
     /** @var  array */
@@ -19,459 +19,221 @@ class FlashMessages
     /** @var  array */
     private $_errorMessages;
 
-
     /**
      * Class Constructor
      */
     public function __construct()
     {
-        $this->_infoMessages    = array();
-        $this->_warningMessages = array();
-        $this->_successMessages = array();
-        $this->_errorMessages   = array();
+        $this->_infoMessages = [];
+        $this->_warningMessages = [];
+        $this->_successMessages = [];
+        $this->_errorMessages = [];
     }
 
-
     /**
-     * This function stores the informational flash message. If $storeInSession
-     * is set to true, then the message is stored in the session. Otherwise, it
-     * is stored locally.
+     * This function stores the informational flash message. If $storeInSession is set to true,
+     * then the message is stored in the session. Otherwise, it is stored locally.
      *
-     * @param string  $key
-     * @param string  $msg
-     * @param bool    $storeInSession
+     * @param string $message
+     * @param bool   $storeInSession
      */
-    public function storeInfoMessage($key, $msg, $storeInSession = false)
+    public function storeInfoMessage($message, $storeInSession = true)
     {
         if($storeInSession == false)
         {
-            if(strlen($key) > 0)
-            {
-                $this->_infoMessages[$key] = $msg;
-            }
-            else
-            {
-                $this->_infoMessages[] = $msg;
-            }
+            $this->_infoMessages[] = $message;
         }
         else
         {
-            $sess = new Session();
-            $infos = $sess->read('flash_infos');
-
-            if(is_array($infos) == false)
-            {
-                $infos = array();
-            }
-
-            if(strlen($key) > 0)
-            {
-                $infos[$key] = $msg;
-            }
-            else
-            {
-                $infos[] = $msg;
-            }
-
-            $sess->write('flash_infos', $infos);
+            $messageType = self::INFO_SESSION_KEY;
+            $this->storeMessageInSession($message, $messageType);
         }
     }
 
+    /**
+     * @param $message
+     * @param $messageType
+     */
+    private function storeMessageInSession($message, $messageType)
+    {
+        $session = new Session();
+        $messages = $session->read($messageType);
+
+        if(is_array($messages) == false)
+        {
+            $messages = [];
+        }
+
+        $messages[] = $message;
+        $session->write($messageType, $messages);
+    }
 
     /**
-     * This function returns an array of all info messages. Additionally,
-     * it deletes all of the info messages.
-     *
-     * @return array
+     * @return array Returns an array of the "info" messages.
      */
     public function retrieveInfoMessages()
     {
-        $msgsLocal = $this->_infoMessages;
-        $this->_infoMessages = array();
+        $localMessages = $this->retrieveAndClearLocalInfoMessages();
+        $sessionMessages = $this->retrieveAndClearSessionMessages(self::INFO_SESSION_KEY);
+        $infoMessages = array_merge($localMessages, $sessionMessages);
 
-        $sess = new Session();
-        $msgsSession = $sess->read('flash_infos');
-        $sess->unsetSessionValue('flash_infos');
-
-        if(is_array($msgsSession) == false)
-        {
-            $msgsSession = array();
-        }
-
-        $msgs = array_merge($msgsLocal, $msgsSession);
-
-        if(count($msgs) == 0)
-        {
-            return null;
-        }
-
-        return $msgs;
+        return $infoMessages;
     }
 
-
     /**
-     * This function returns the info message specified by $key. Addiitonally,
-     * it deletes the specified info message from the flass message store.
-     *
-     * @param $key
-     * @return string
+     * @return array
      */
-    public function retrieveInfoMessage($key)
+    private function retrieveAndClearLocalInfoMessages()
     {
-        if(array_key_exists($key, $this->_infoMessages) == true)
-        {
-            $msg = $this->_infoMessages[$key];
-            unset($this->_infoMessages[$key]);
+        $localMessages = $this->_infoMessages;
+        $this->_infoMessages = [];
 
-            return $msg;
-        }
-
-        $sess = new Session();
-        $infos = $sess->read('flash_infos');
-
-        if(is_array($infos) && array_key_exists($key, $infos) == true)
-        {
-            $msg = $infos[$key];
-            unset($infos[$key]);
-
-            $sess->write('flash_infos', $infos);
-
-            return $msg;
-        }
-
-        return null;
+        return $localMessages;
     }
 
+    /**
+     * @param string $messageType
+     *
+     * @return array
+     */
+    private function retrieveAndClearSessionMessages($messageType)
+    {
+        $session = new Session();
+        $sessionMessages = $session->read($messageType);
+        $session->unsetSessionValue($messageType);
+
+        if(is_array($sessionMessages) == false)
+        {
+            $sessionMessages = [];
+        }
+
+        return $sessionMessages;
+    }
 
     /**
-     * This function stores the success flash message. If $storeInSession
-     * is set to true, then the message is stored in the session. Otherwise, it
-     * is stored locally.
+     * This function stores a success flash message. If $storeInSession is set to true, then
+     * the message is stored in the session. Otherwise, it is stored locally.
      *
-     * @param string  $key
-     * @param string  $msg
-     * @param bool    $storeInSession
+     * @param string $message
+     * @param bool   $storeInSession
      */
-    public function storeSuccessMessage($key, $msg, $storeInSession = false)
+    public function storeSuccessMessage($message, $storeInSession = true)
     {
         if($storeInSession == false)
         {
-            if(strlen($key) > 0)
-            {
-                $this->_successMessages[$key] = $msg;
-            }
-            else
-            {
-                $this->_successMessages[] = $msg;
-            }
+            $this->_successMessages[] = $message;
         }
         else
         {
-            $sess = new Session();
-            $successes = $sess->read('flash_successes');
-
-            if(is_array($successes) == false)
-            {
-                $successes = array();
-            }
-
-            if(strlen($key) > 0)
-            {
-                $successes[$key] = $msg;
-            }
-            else
-            {
-                $successes[] = $msg;
-            }
-
-            $sess->write('flash_successes', $successes);
+            $messageType = self::SUCCESS_SESSION_KEY;
+            $this->storeMessageInSession($message, $messageType);
         }
     }
 
-
     /**
-     * This function returns an array of all success messages. Additionally,
-     * it deletes all of the success messages.
-     *
-     * @return array
+     * @return array Returns an array of the "success" messages.
      */
     public function retrieveSuccessMessages()
     {
-        $msgsLocal = $this->_successMessages;
+        $localMessages = $this->retrieveLocalSuccessMessages();
+        $sessionMessages = $this->retrieveAndClearSessionMessages(self::SUCCESS_SESSION_KEY);
+        $messages = array_merge($localMessages, $sessionMessages);
+
+        return $messages;
+    }
+
+    /**
+     * @return array
+     */
+    private function retrieveLocalSuccessMessages()
+    {
+        $localMessages = $this->_successMessages;
         $this->_successMessages = array();
 
-        $sess = new Session();
-        $msgsSession = $sess->read('flash_successes');
-        $sess->unsetSessionValue('flash_successes');
-
-        if(is_array($msgsSession) == false)
-        {
-            $msgsSession = array();
-        }
-
-        $msgs = array_merge($msgsLocal, $msgsSession);
-
-        if(count($msgs) == 0)
-        {
-            return null;
-        }
-
-        return $msgs;
+        return $localMessages;
     }
 
-
     /**
-     * This function returns the success message specified by $key. Addiitonally,
-     * it deletes the specified success message from the flass message store.
+     * This function stores the "warning" flash message. If $storeInSession is set to true, then
+     * the message is stored in the session. Otherwise, it is stored locally.
      *
-     * @param $key
-     * @return string
+     * @param string $msg
+     * @param bool   $storeInSession
      */
-    public function retrieveSuccessMessage($key)
-    {
-        if(array_key_exists($key, $this->_successMessages) == true)
-        {
-            $msg = $this->_successMessages[$key];
-            unset($this->_successMessages[$key]);
-
-            return $msg;
-        }
-
-        $sess = new Session();
-        $successes = $sess->read('flash_successes');
-
-        if(is_array($successes) && array_key_exists($key, $successes) == true)
-        {
-            $msg = $successes[$key];
-            unset($successes[$key]);
-
-            $sess->write('flash_successes', $successes);
-
-            return $msg;
-        }
-
-        return null;
-    }
-
-
-    /**
-     * This function stores the warning flash message. If $storeInSession
-     * is set to true, then the message is stored in the session. Otherwise, it
-     * is stored locally.
-     *
-     * @param string  $key
-     * @param string  $msg
-     * @param bool    $storeInSession
-     */
-    public function storeWarningMessage($key, $msg, $storeInSession = false)
+    public function storeWarningMessage($msg, $storeInSession = true)
     {
         if($storeInSession == false)
         {
-            if(strlen($key) > 0)
-            {
-                $this->_warningMessages[$key] = $msg;
-            }
-            else
-            {
-                $this->_warningMessages[] = $msg;
-            }
+            $this->_warningMessages[] = $msg;
         }
         else
         {
-            $sess = new Session();
-            $warnings = $sess->read('flash_warnings');
-
-            if(is_array($warnings) == false)
-            {
-                $warnings = array();
-            }
-
-            if(strlen($key) > 0)
-            {
-                $warnings[$key] = $msg;
-            }
-            else
-            {
-                $warnings[] = $msg;
-            }
-
-            $sess->write('flash_warnings', $warnings);
+            $this->storeMessageInSession($msg, self::WARNING_SESSION_KEY);
         }
     }
 
-
     /**
-     * This function returns an array of all warning messages. Additionally,
-     * it deletes all of the warning messages.
-     *
-     * @return array
+     * @return array Returns an array of the "warning" messages.
      */
     public function retrieveWarningMessages()
     {
-        $msgsLocal = $this->_warningMessages;
+        $localMessages = $this->retrieveLocalWarningMessages();
+        $sessionMessages = $this->retrieveAndClearSessionMessages(self::WARNING_SESSION_KEY);
+        $messages = array_merge($localMessages, $sessionMessages);
+
+        return $messages;
+    }
+
+    /**
+     * @return array
+     */
+    private function retrieveLocalWarningMessages()
+    {
+        $localMessages = $this->_warningMessages;
         $this->_warningMessages = array();
 
-        $sess = new Session();
-        $msgsSession = $sess->read('flash_warnings');
-        $sess->unsetSessionValue('flash_warnings');
-
-        if(is_array($msgsSession) == false)
-        {
-            $msgsSession = array();
-        }
-
-        $msgs = array_merge($msgsLocal, $msgsSession);
-
-        if(count($msgs) == 0)
-        {
-            return null;
-        }
-
-        return $msgs;
+        return $localMessages;
     }
 
-
     /**
-     * This function returns the warning message specified by $key. Addiitonally,
-     * it deletes the specified warning message from the flass message store.
+     * This function stores the "error" flash message. If $storeInSession is set to true, then
+     * the message is stored in the session. Otherwise, it is stored locally.
      *
-     * @param $key
-     * @return string
+     * @param string $message
+     * @param bool   $storeInSession
      */
-    public function retrieveWarningMessage($key)
-    {
-        if(array_key_exists($key, $this->_warningMessages) == true)
-        {
-            $msg = $this->_warningMessages[$key];
-            unset($this->_warningMessages[$key]);
-
-            return $msg;
-        }
-
-        $sess = new Session();
-        $warnings = $sess->read('flash_warnings');
-
-        if(is_array($warnings) && array_key_exists($key, $warnings) == true)
-        {
-            $msg = $warnings[$key];
-            unset($warnings[$key]);
-
-            $sess->write('flash_warnings', $warnings);
-
-            return $msg;
-        }
-
-        return null;
-    }
-
-
-    /**
-     * This function stores the error flash message. If $storeInSession
-     * is set to true, then the message is stored in the session. Otherwise, it
-     * is stored locally.
-     *
-     * @param string  $key
-     * @param string  $msg
-     * @param bool    $storeInSession
-     */
-    public function storeErrorMessage($key, $msg, $storeInSession = false)
+    public function storeErrorMessage($message, $storeInSession = true)
     {
         if($storeInSession == false)
         {
-            if(strlen($key) > 0)
-            {
-                $this->_errorMessages[$key] = $msg;
-            }
-            else
-            {
-                $this->_errorMessages[] = $msg;
-            }
+            $this->_errorMessages[] = $message;
         }
         else
         {
-            $sess = new Session();
-            $errors = $sess->read('flash_errors');
-
-            if(is_array($errors) == false)
-            {
-                $errors = array();
-            }
-
-            if(strlen($key) > 0)
-            {
-                $errors[$key] = $msg;
-            }
-            else
-            {
-                $errors[] = $msg;
-            }
-
-            $sess->write('flash_errors', $errors);
+            $this->storeMessageInSession($message, self::ERROR_SESSION_KEY);
         }
     }
 
-
     /**
-     * This function returns an array of all error messages. Additionally,
-     * it deletes all of the error messages.
-     *
-     * @return array
+     * @return array Returns an array of all "error" messages.
      */
     public function retrieveErrorMessages()
     {
-        $msgsLocal = $this->_errorMessages;
-        $this->_errorMessages = array();
+        $localMessages = $this->retrieveAndClearLocalErrorMessages();
+        $sessionMessages = $this->retrieveAndClearSessionMessages(self::ERROR_SESSION_KEY);
+        $messages = array_merge($localMessages, $sessionMessages);
 
-        $sess = new Session();
-        $msgsSession = $sess->read('flash_errors');
-        $sess->unsetSessionValue('flash_errors');
-
-        if(is_array($msgsSession) == false)
-        {
-            $msgsSession = array();
-        }
-
-        $msgs = array_merge($msgsLocal, $msgsSession);
-
-        if(count($msgs) == 0)
-        {
-            return null;
-        }
-
-        return $msgs;
+        return $messages;
     }
 
-
     /**
-     * This function returns the error message specified by $key. Addiitonally,
-     * it deletes the specified error message from the flass message store.
-     *
-     * @param $key
-     * @return string
+     * @return array
      */
-    public function retrieveErrorMessage($key)
+    private function retrieveAndClearLocalErrorMessages()
     {
-        if(array_key_exists($key, $this->_errorMessages) == true)
-        {
-            $msg = $this->_errorMessages[$key];
-            unset($this->_errorMessages[$key]);
+        $localMessages = $this->_errorMessages;
+        $this->_errorMessages = array();
 
-            return $msg;
-        }
-
-        $sess = new Session();
-        $errors = $sess->read('flash_errors');
-
-        if(is_array($errors) && array_key_exists($key, $errors) == true)
-        {
-            $msg = $errors[$key];
-            unset($errors[$key]);
-
-            $sess->write('flash_errors', $errors);
-
-            return $msg;
-        }
-
-        return null;
+        return $localMessages;
     }
 }
--- tests/JoeFallon/PhpFlash/FlashMessagesTests.php
+++ tests/JoeFallon/PhpFlash/FlashMessagesTests.php
@@ -4,14 +4,9 @@ namespace tests\JoeFallon\PhpFlash;
 use JoeFallon\KissTest\UnitTest;
 use JoeFallon\PhpFlash\FlashMessages;
 
-/**
- * @author    Joseph Fallon <joseph.t.fallon@gmail.com>
- * @copyright Copyright 2014 Joseph Fallon (All rights reserved)
- * @license   MIT
- */
 class FlashMessagesTests extends UnitTest
 {
-    public function test_ctor_properly_initializes_instance()
+    public function test_initialization()
     {
         $flash           = new FlashMessages();
         $errorMessages   = $flash->retrieveErrorMessages();
@@ -19,157 +14,66 @@ class FlashMessagesTests extends UnitTest
         $successMessages = $flash->retrieveSuccessMessages();
         $warningMessages = $flash->retrieveWarningMessages();
 
-        $this->assertEqual($errorMessages,   null, '$errorMessages');
-        $this->assertEqual($infoMessages,    null, '$infoMessages');
-        $this->assertEqual($successMessages, null, '$successMessages');
-        $this->assertEqual($warningMessages, null, '$warningMessages');
+        $this->assertTrue(is_array($errorMessages));
+        $this->assertTrue(is_array($infoMessages));
+        $this->assertTrue(is_array($successMessages));
+        $this->assertTrue(is_array($warningMessages));
+
+        $this->assertEqual(count($errorMessages),   0, '$errorMessages');
+        $this->assertEqual(count($infoMessages),    0, '$infoMessages');
+        $this->assertEqual(count($successMessages), 0, '$successMessages');
+        $this->assertEqual(count($warningMessages), 0, '$warningMessages');
     }
 
-    public function test_store_local_error_message_is_retrieve_by_key_and_deleted()
+    public function test_retrieveErrorMessages_retrieves_messages_and_deletes()
     {
         $flash = new FlashMessages();
-        $flash->storeErrorMessage('test_err_msg', 'test error message');
-
-        $out1 = $flash->retrieveErrorMessage('test_err_msg');
-        $this->assertEqual($out1, 'test error message');
-
-        $out2 = $flash->retrieveErrorMessage('test_err_msg');
-        $this->assertEqual($out2, null);
-    }
-
-    public function test_store_local_info_message_is_retrieve_by_key_and_deleted()
-    {
-        $flash = new FlashMessages();
-        $flash->storeInfoMessage('test_info_msg', 'test info message');
-
-        $out1 = $flash->retrieveInfoMessage('test_info_msg');
-        $this->assertEqual($out1, 'test info message');
-
-        $out2 = $flash->retrieveInfoMessage('test_info_msg');
-        $this->assertEqual($out2, null);
-    }
-
-    public function test_store_local_success_message_is_retrieve_by_key_and_deleted()
-    {
-        $flash = new FlashMessages();
-        $flash->storeSuccessMessage('test_success_msg', 'test success message');
-
-        $out1 = $flash->retrieveSuccessMessage('test_success_msg');
-        $this->assertEqual($out1, 'test success message');
-
-        $out2 = $flash->retrieveSuccessMessage('test_success_msg');
-        $this->assertEqual($out2, null);
-    }
-
-    public function test_store_local_warning_message_is_retrieve_by_key_and_deleted()
-    {
-        $flash = new FlashMessages();
-        $flash->storeWarningMessage('test_warn_msg', 'test warn message');
-
-        $out1 = $flash->retrieveWarningMessage('test_warn_msg');
-        $this->assertEqual($out1, 'test warn message');
-
-        $out2 = $flash->retrieveWarningMessage('test_warn_msg');
-        $this->assertEqual($out2, null);
-    }
-
-    public function test_store_session_error_message_is_retrieve_by_key_and_deleted()
-    {
-        $flash = new FlashMessages();
-        $flash->storeErrorMessage('test_err_msg', 'test error message', true);
-
-        $out1 = $flash->retrieveErrorMessage('test_err_msg');
-        $this->assertEqual($out1, 'test error message');
-
-        $out2 = $flash->retrieveErrorMessage('test_err_msg');
-        $this->assertEqual($out2, null);
-    }
-
-    public function test_store_session_info_message_is_retrieve_by_key_and_deleted()
-    {
-        $flash = new FlashMessages();
-        $flash->storeInfoMessage('test_info_msg', 'test info message', true);
-
-        $out1 = $flash->retrieveInfoMessage('test_info_msg');
-        $this->assertEqual($out1, 'test info message');
-
-        $out2 = $flash->retrieveInfoMessage('test_info_msg');
-        $this->assertEqual($out2, null);
-    }
-
-    public function test_store_session_success_message_is_retrieve_by_key_and_deleted()
-    {
-        $flash = new FlashMessages();
-        $flash->storeSuccessMessage('test_success_msg', 'test success message', true);
-
-        $out1 = $flash->retrieveSuccessMessage('test_success_msg');
-        $this->assertEqual($out1, 'test success message');
-
-        $out2 = $flash->retrieveSuccessMessage('test_success_msg');
-        $this->assertEqual($out2, null);
-    }
-
-    public function test_store_session_warning_message_is_retrieve_by_key_and_deleted()
-    {
-        $flash = new FlashMessages();
-        $flash->storeWarningMessage('test_warn_msg', 'test warn message', true);
-
-        $out1 = $flash->retrieveWarningMessage('test_warn_msg');
-        $this->assertEqual($out1, 'test warn message');
-
-        $out2 = $flash->retrieveWarningMessage('test_warn_msg');
-        $this->assertEqual($out2, null);
-    }
-
-    public function test_retrieveErrorMessages_retrieves_msgs_and_deletes()
-    {
-        $flash = new FlashMessages();
-        $flash->storeErrorMessage('test_err_msg1', 'test error message1');
-        $flash->storeErrorMessage('test_err_msg2', 'test error message2', true);
+        $flash->storeErrorMessage('test error message1');
+        $flash->storeErrorMessage('test error message2', true);
 
         $out1 = $flash->retrieveErrorMessages();
         $this->assertEqual(count($out1), 2);
 
         $out2 = $flash->retrieveErrorMessages();
-        $this->assertEqual($out2, null);
+        $this->assertEqual(count($out2), 0);
     }
 
-    public function test_retrieveInfoMessages_retrieves_msgs_and_deletes()
+    public function test_retrieveInfoMessages_retrieves_messages_and_deletes()
     {
         $flash = new FlashMessages();
-        $flash->storeInfoMessage('test_info_msg1', 'test info message1');
-        $flash->storeInfoMessage('test_info_msg2', 'test info message2', true);
+        $flash->storeInfoMessage('test info message1');
+        $flash->storeInfoMessage('test info message2', true);
 
         $out1 = $flash->retrieveInfoMessages();
         $this->assertEqual(count($out1), 2);
 
         $out2 = $flash->retrieveInfoMessages();
-        $this->assertEqual($out2, null);
+        $this->assertEqual(count($out2), 0);
     }
 
-    public function test_retrieveSuccessMessages_retrieves_msgs_and_deletes()
+    public function test_retrieveSuccessMessages_retrieves_messages_and_deletes()
     {
         $flash = new FlashMessages();
-        $flash->storeSuccessMessage('test_success_msg1', 'test success message1');
-        $flash->storeSuccessMessage('test_success_msg2', 'test success message2', true);
+        $flash->storeSuccessMessage('test success message1');
+        $flash->storeSuccessMessage('test success message2', true);
 
         $out1 = $flash->retrieveSuccessMessages();
         $this->assertEqual(count($out1), 2);
 
         $out2 = $flash->retrieveSuccessMessages();
-        $this->assertEqual($out2, null);
+        $this->assertEqual(count($out2), 0);
     }
 
-    public function test_retrieveWarningMessages_retrieves_msgs_and_deletes()
+    public function test_retrieveWarningMessages_retrieves_messages_and_deletes()
     {
         $flash = new FlashMessages();
-        $flash->storeWarningMessage('test_warn_msg1', 'test warn message1');
-        $flash->storeWarningMessage('test_warn_msg2', 'test warn message2', true);
+        $flash->storeWarningMessage('test warn message1');
+        $flash->storeWarningMessage('test warn message2', true);
 
         $out1 = $flash->retrieveWarningMessages();
         $this->assertEqual(count($out1), 2, 'count');
 
         $out2 = $flash->retrieveWarningMessages();
-        $this->assertEqual($out2, null, 'null check');
+        $this->assertEqual(count($out2), 0);;
     }
 }
